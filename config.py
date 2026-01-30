7b25c15619a023396c7f22232e76f2a16148164e
N4NDOCO <andersonsilva978x@gmail.com>
2026-01-30 11:52:36 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+TOKEN = os.environ.get("TOKEN")
+
+GUILD_ID = 1465477542919016625
+
+CALL_CHANNEL_ID = 1465657430292697151  # ✅┃escolha-e-pagamentos
+
+ROLE_ENTREGADOR_NAME = "Entregador"
+
+CATEGORY_NAME = "Tickets"
